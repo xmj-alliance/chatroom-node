7f84bad15088f472c44f8a1c635c0e8ffeff54ae
Chenxuan Wang <wcx@xtoneict.com>
2017-09-21 15:48:55 +0800

    + 写了个小模板便于快速新增component   （其实并没有angular-cli方便但是灵活）

---
+++ src/client/app/dummy/dummy.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+	selector: 'chat-dummy',
+	templateUrl: './dummy.component.html',
+	styleUrls: ['./dummy.component.scss']
+})
+export class DummyComponent implements OnInit {
+	constructor(
+	) { }
+	
+	ngOnInit() {}
+
+}
---
+++ src/client/app/dummy/dummy.module.ts
@@ -0,0 +1,15 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+// componets
+import { DummyComponent } from './dummy.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [
+    DummyComponent
+  ]
+})
+export class DummyModule { }
