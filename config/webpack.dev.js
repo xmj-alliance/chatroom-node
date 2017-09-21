814a9c2403b904e57bfb19a7afadcfc1fc2a6775
Chenxuan Wang <wcx@xtoneict.com>
2017-09-21 11:45:23 +0800

    + 引入了Bootstrap4（仅网格系统）   * 同时在dev配置中分开了全局scss与局部scss的打包模式。局部scss要to-string-loader， 但是全局scss要style-loader。 在prod配置中两者相同，最后都是打包成一个css文件。

--- config/webpack.dev.js
+++ config/webpack.dev.js
@@ -1,7 +1,16 @@
+const path = require('path');
+
 const webpackMerge = require('webpack-merge');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const commonConfig = require('./webpack.common.js');
 const helpers = require('./helpers');
+
+const clientpath = path.join(__dirname, "../src/client");
+const appPath = path.resolve(clientpath, 'app');
+const globalscss = [
+  path.resolve(clientpath, 'style.scss')
+]
+
 
 module.exports = webpackMerge(commonConfig, {
   devtool: 'cheap-module-eval-source-map',
@@ -9,10 +18,28 @@ module.exports = webpackMerge(commonConfig, {
   module: {
     rules: [
       {
+        /* Scoped scss */
         test: /\.scss$/,
+        exclude: globalscss, // exclude global scss
         use: [
           {
-            loader: "to-string-loader"
+            loader: "to-string-loader" // Angular needs to-string instead of style-loader
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      },
+      {
+        /* Global scss */
+        test: /\.scss$/,
+        exclude: appPath, // exclude scoped styles
+        use: [
+          {
+            loader: "style-loader" // global styles needs to get injected to <style></style>
           },
           {
             loader: "css-loader" // translates CSS into CommonJS
