cdaa954561569fe3e91ab94112b68df448b491a9
Valorad <wcxaaa@gmail.com>
2017-09-27 20:33:01 +0800

    + involved logger

--- src/server/chatroom-node.ts
+++ src/server/chatroom-node.ts
@@ -3,7 +3,7 @@ import * as path from 'path';
 import * as Koa from 'koa';
 import * as Router from 'koa-router';
 // import * as favicon from 'serve-favicon';
-// import * as logger from 'morgan';
+import * as logger from 'koa-logger';
 // import * as cookieParser from 'cookie-parser';
 // import * as bodyParser from 'body-parser';
 // import * as cors from 'cors';
@@ -14,6 +14,8 @@ import { api } from './router/api';
 let app = new Koa();
 let router = new Router();
 
+app.use(logger());
+
 // root route and sub route settings
 router.get('/', async (ctx, next) => {
   ctx.body="root"
@@ -21,9 +23,11 @@ router.get('/', async (ctx, next) => {
 router.use('/api', api.routes(), api.allowedMethods())
 
 app.use(router.routes())
-      .use(router.allowedMethods());
+    .use(router.allowedMethods());
+
+// listen
 app.listen(3000, () => {
-  console.log("koa started at 3000");
+  console.log("** koa started on port 3000. **");
 });
 
 export default app;
