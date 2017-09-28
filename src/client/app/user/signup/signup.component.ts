a445f49f7fc5ed32699f99466fa9bd577b863912
wcxaaa <wcx@xtoneict.com>
2017-09-28 13:59:31 +0800

    + ng form validation on eail password and passconfirm * form based on form builder (with its controller), in order to impement password conformation

--- src/client/app/user/signup/signup.component.ts
+++ src/client/app/user/signup/signup.component.ts
@@ -1,4 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+import { matchOtherValidator } from "../../_utils/match-other-validator";
 
 @Component({
 	selector: 'user-signup',
@@ -7,14 +10,51 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignupComponent implements OnInit {
 	constructor(
-	) { }
+		@Inject(FormBuilder) fb: FormBuilder
+	) {
+		this.constructForm(fb);
+	}
 	
-	ngOnInit() {}
+	ngOnInit() {
+		
+	}
+
+	repeatPassword: string = null;
+	form: FormGroup;
 
 	newUser: any = {
 		email: null,
-		password: null,
-		passwordConfirm: null
+		password: null
 	}
+
+
+
+	constructForm (fb: FormBuilder) {
+		this.form = fb.group({
+			email: ['', [
+				Validators.required,
+				Validators.email
+			]],
+			password: ['', [
+				Validators.required,
+				Validators.minLength(6)
+			]],
+			repeatPassword: ['', [
+				Validators.required,
+				matchOtherValidator('password')
+			]]
+		});
+	}
+
+	signup: ()=>any = () => {
+		console.log(this.form.controls);
+		if (!this.form.invalid) {
+			
+			console.log(this.newUser);
+		} else {
+			console.log("Your form is invalid. Submission aborted.");
+		}
+
+	};
 
 }
--- src/client/app/user/user.module.ts
+++ src/client/app/user/user.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule  } from '@angular/forms';
 
 // md modules
 import { MatInputModule } from '@angular/material';
@@ -30,6 +30,7 @@ const mdModules = [
   imports: [
     CommonModule,
     FormsModule,
+    ReactiveFormsModule,
 
     mdModules,
 
