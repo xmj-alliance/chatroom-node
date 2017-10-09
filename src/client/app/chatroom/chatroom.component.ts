c058f221329f8698ad0e8dd2d88ad33102ba118c
wcxaaa <wcx@xtoneict.com>
2017-10-09 11:42:20 +0800

    + designed chat list

--- src/client/app/chatroom/chatroom.component.ts
+++ src/client/app/chatroom/chatroom.component.ts
@@ -11,4 +11,87 @@ export class ChatroomComponent implements OnInit {
 	
 	ngOnInit() {}
 
+	chatList: any[] = [
+		{
+			name: "kde",
+			time: new Date("2017-08-08"),
+			avatar: "kde.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "gnome",
+			time: new Date("2017-08-08"),
+			avatar: "gnome.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "elementary",
+			time: new Date("2017-08-08"),
+			avatar: "elementary.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "macos",
+			time: new Date("2017-08-08"),
+			avatar: "macos.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "windows",
+			time: new Date("2017-08-08"),
+			avatar: "windows.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "cinemon",
+			time: new Date("2017-08-08"),
+			avatar: "cinemon.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "lxqt",
+			time: new Date("2017-08-08"),
+			avatar: "lxqt.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "xfce",
+			time: new Date("2017-08-08"),
+			avatar: "xfce.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "github",
+			time: new Date("2017-08-08"),
+			avatar: "github.jpg",
+			briefText: "some last words",
+			selected: false
+		},
+		{
+			name: "luci",
+			time: new Date("2017-08-08"),
+			avatar: "luci.jpg",
+			briefText: "some last words",
+			selected: false
+		}
+	];
+
+	// search a chat in the given list. Chat items are normally unique.
+	private searchChatByName = (chatName: string) => {
+		for (let chat of this.chatList) {
+			if (chat.name === chatName) {
+				return chat;
+			}
+		}
+		return null;
+	};
+
 }
